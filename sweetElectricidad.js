ddb2d0b3ffce2f45a03d93eb8b6e35401ac95de4
emiliorubio <emiliormadariaga@gmail.com>
2023-11-09 00:19:00 -0500

    se modifica presupuesto

---
+++ sweetElectricidad.js
@@ -0,0 +1,8 @@
+(async() => {
+    await  Swal.fire({
+        title: `Todas nuestras instalaciones estan certificadas y bajo norma establecida por chile`,
+		icon:'info',
+        timer: 5000,
+	    timerProgressBar:true,
+    })
+})() 
